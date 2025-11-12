2ab4e0aaa5d5aaa658c8f57e8c9658b6f557aa91
mohabbaticicle <jessijesica217@gmail.com>
2025-11-13 04:41:47 +0600

    FramerMotionNotWorks

---
+++ src/app/components/BackgroundMotion.jsx
@@ -0,0 +1,8 @@
+export default function BackgroundMotion() {
+  return (
+    <div className="fixed inset-0 -z-10 bg-red-500">
+      {/* This should cover the entire screen in red */}
+      TEST BACKGROUND - YOU SHOULD SEE RED
+    </div>
+  );
+}
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,27 @@
 import { Orbitron } from "next/font/google";
 import "./globals.css";
+import BackgroundMotion from "./components/BackgroundMotion";
 
+const orbitron = Orbitron({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+  variable: "--font-orbitron",
+});
 
 export const metadata = {
   title: "Tanmoy | Developer Portfolio",
   description: "Welcome to Tanmoy's Portfolio",
 };
 
-const orbitron = Orbitron({
-  subsets: ["latin"],
-  weight: ["400", "700"], // choose the weights you need
-  variable: "--font-orbitron",
-});
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${orbitron.variable} antialiased`}>
-        {children}
+      <body className={`${orbitron.variable} antialiased relative bg-black`}>
+        {/* Background animation */}
+        <BackgroundMotion />
+
+        {/* Main content */}
+        <main className="relative z-10">{children}</main>
       </body>
     </html>
   );
